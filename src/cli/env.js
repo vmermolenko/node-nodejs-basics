e36af0b68b15be800ca9ee2dd699fe36397bd30b
Vadim <vmermolenko.dev@gmail.com>
2022-06-01 21:44:27 +0300

    first commit

--- src/cli/env.js
+++
@@ -1,8 +0,0 @@
-export const parseEnv = () => {
-	const env = process.env;
-	let envRSS = ''
-	Object.keys(env).forEach(el => {
-		if (el.slice(0, 4) === 'RSS_')  envRSS += el +"="+env[el] +";";
-	});
-	console.log(envRSS)
-};
--- src/fs/rename.js
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-export const rename = async () => {
-    // Write your code here 
-    const oldPath = 'src/fs/files/wrongFilename.txt';
-    const newPath = 'src/fs/files/properFilename.md';
-
-    if (!fs.existsSync(oldPath) || fs.existsSync(newPath)) throw new Error('FS operation failed');
-    fs.renameSync(oldPath, newPath);
-};
